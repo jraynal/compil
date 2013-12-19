ae647da12891ada214a25b7a9b377d6dbc168fc4
ebrelle <ebrelle@enseirb.fr>
2013-12-19 13:27:16 +0100

    les joies de la grammaire

---
+++ programm_example.c
@@ -0,0 +1,43 @@
+
+
+int fonction1 ()
+{}
+
+void fonction2 ()()[]
+{}
+
+//le corps des fonctions et tableaux se fait n'importe ou dans les prototypes
+int (fonction3(int arg1))
+{
+	;
+	{
+		//declaration sont toujours en tete de programme
+		// on affecte pas pendant la decla
+		int var();
+		float var3;
+
+		++ ++ -- var ;
+		- - - var2 = - var3 ++;
+		
+
+
+		for (i ++ [1.5];var(1,5.7,2.3< 0);va4); //de meme avec les if et if else
+
+	}
+}
+
+float tableau0 []
+{}
+
+void (tableau1 [])
+{}
+
+void tableau2 []()()
+{}
+
+
+//on peut declarer diffrents objets dans la meme lignes 
+float tableau3[], var, fonction() , truc()(), bidule(int arg1, void (arg2 []));
+int tableau3[], var, fonction() , truc()();
+
+
